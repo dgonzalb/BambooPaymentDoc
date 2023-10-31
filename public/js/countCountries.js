e22a20a553135bcbaab08b664583ab8b4e392cea
JRiveroBamboo <126700679+JRiveroBamboo@users.noreply.github.com>
2023-10-31 12:48:12 -0500

    Publish October 31, 2023

--- public/js/countCountries.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    var phtml = document.getElementById('available-countries').nextSibling.nextSibling.innerHTML.replace('MM', document.getElementsByClassName('map-point').length);
-    document.getElementById('available-countries').nextSibling.nextSibling.innerHTML = phtml;
-}
